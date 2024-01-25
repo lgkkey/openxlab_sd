05fac5c6f06a6f106d0f353289d8525e12d23919
lgkkey <41468382+lgkkey@users.noreply.github.com>
2024-01-25 20:16:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
 import os
 os.system("pwd")
 os.system("ls -la")
-os.system(f"python app1.py")
+os.system("rm -rf app_start.py")
+os.system("rm -rf openxlab_start")
+os.system("ls -la")
+print("--------------------------------------")
+os.system("git clone https://github.com/lgkkey/openxlab_start.git")
+if os.path.exists("openxlab_start"):
+    os.system("cp openxlab_start/app_start.py ./")
+else:
+    os.system("git clone https://github.com/lgkkey/openxlab_start.git")
+    
+if os.path.exists("app_start.py"):
+    print("app_start.py exists")
+
+os.system("ls -la")
+print("----------------start app_start.py----------------------")
+
+os.system(f"python app_start.py")
